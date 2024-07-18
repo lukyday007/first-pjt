d1b760bd6669e52edf882be5eb9d2e9ed8a46ab8
최태민 <wxogud3377@naver.com>
2024-07-18 14:18:14 +0900

    refactor: 코드리뷰 수정

--- BackEnd/src/main/java/com/boricori/entity/GameRoom.java
+++ BackEnd/src/main/java/com/boricori/entity/GameRoom.java
@@ -3,6 +3,8 @@ package com.boricori.entity;
 import com.boricori.dto.request.gameroom.setting.GameSettingRequest;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
@@ -11,12 +13,13 @@ import java.util.List;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class GameRoom {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name="game_id")
-    private int id;
+    private Long id;
 
     @Column(length = 64, nullable = false)
     private String roomName;
@@ -36,6 +39,7 @@ public class GameRoom {
     private LocalDateTime endTime;
     private String codeNumber;
 
+    @Builder
     public GameRoom(String roomName, int limit, int mapSize, boolean magneticField, String codeNumber) {
         this.roomName = roomName;
         this.limit = limit;
