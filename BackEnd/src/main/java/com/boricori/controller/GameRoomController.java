78abfb043e3a3e629b498d5a5bda55c4993167df
최태민 <wxogud3377@naver.com>
2024-07-23 13:22:58 +0900

    S11P12B205-184 <fix: 방생성 기능 및 방 세팅 정보 반환>

--- BackEnd/src/main/java/com/boricori/controller/GameRoomController.java
+++ BackEnd/src/main/java/com/boricori/controller/GameRoomController.java
@@ -1,31 +1,21 @@
 package com.boricori.controller;
 
-import com.boricori.dto.request.gameroom.EndGameRoomRequest;
-import com.boricori.dto.request.gameroom.GameRequest;
-import com.boricori.dto.request.gameroom.GameUpdateRequest;
-import com.boricori.dto.request.gameroom.PlayerInfoRequest;
-import com.boricori.dto.request.gameroom.StartGameRoomRequest;
+import com.boricori.dto.request.gameroom.*;
 import com.boricori.dto.response.gameroom.CreateGameRoomResponse;
 import com.boricori.dto.response.gameroom.GameRoomSettingResponse;
 import com.boricori.dto.response.gameroom.StartGameRoomResponse;
 import com.boricori.dto.response.gameroom.end.EndGameResponse;
-import com.boricori.entity.GameRoom;
 import com.boricori.service.GameRoomService;
 import com.boricori.service.ParticipantsService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/gameroom")
 @RestController
@@ -37,7 +27,7 @@ public class GameRoomController {
   @Autowired
   private ParticipantsService participantsService;
 
-  @GetMapping("/create")
+  @PostMapping("/create")
   @Operation(summary = "게임방 생성", description = "게임방 생성")
   @ApiResponses({
       @ApiResponse(responseCode = "200", description = "성공"),
@@ -47,7 +37,12 @@ public class GameRoomController {
   public ResponseEntity<GameRoomSettingResponse> createGameRoom(
       @RequestBody @Parameter(description = "회원가입 정보", required = true) GameRequest gameRequest) {
 
-    return null;
+    try {
+      GameRoomSettingResponse gameRoomSettingResponse = gameRoomService.makeRoom(gameRequest);
+      return ResponseEntity.status(200).body(gameRoomSettingResponse);
+    } catch (Exception e) {
+      return ResponseEntity.status(404).body(null);
+    }
   }
 
   @GetMapping("/{id}")
@@ -86,9 +81,9 @@ public class GameRoomController {
   public ResponseEntity<StartGameRoomResponse> startGameRoom(
       @RequestBody @Parameter(description = "게임 시작 서버 데이터 전달", required = true) StartGameRoomRequest request) {
     // 게임 방 튜플 생성
-    GameRoom gameRoom = gameRoomService.makeRoom(request);
+//    GameRoom gameRoom = gameRoomService.makeRoom(request);
     // 게임 참여자 튜플 생성 JPA
-    participantsService.makeGameParticipant(gameRoom, request.getPlayerInfoRequests());
+//    participantsService.makeGameParticipant(gameRoom, request.getPlayerInfoRequests());
     // 생성된 게임 방 id를 받음
     // ..
     // 게임 참여 id에 맞게 꼬리잡기 리스트 생성 Map<int, List<ParticipantNameDto>>
