e13d1c0686877fdff00cda3bfe02ecfc7bf97b52
1139091SSAFY <iiky2124@gmail.com>
2024-08-01 13:47:58 +0900

    feat: 영역 이탈 타이머 기능 구현 [S11P12B205-180]

---
+++ FrontEnd/src/hooks/Map/useTimer.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect, useRef, useCallback, useContext } from "react";
+import { GameContext } from "@/context/GameContext";
+import axiosInstance from "@/api/axiosInstance.js";
+
+const useTimer = initialTime => {
+  const { gameRoomId, userId } = useContext(GameContext);
+  const [time, setTime] = useState(() => {
+    const savedTime = localStorage.getItem("remainingTime");
+    return savedTime !== null ? parseInt(savedTime, 10) : initialTime; // localStorage에 시간 정보가 있으면 사용
+  });
+  const intervalRef = useRef(null);
+
+  const decreaseTime = useCallback(() => {
+    setTime(prevTime => prevTime - 1);
+  }, []);
+
+  useEffect(() => {
+    if (time <= 0) {
+      // 주어진 시간을 모두 소진 시 사망 처리 및 axios
+      (async () => {
+        try {
+          const response = await axiosInstance.patch(
+            `/participants/${gameRoomId}/${userId}/die`
+          );
+          if (response.status == 200) {
+            // 타이머 종료로 인한 사망 후 처리
+          } else {
+            // 타이머 종료 상태를 서버에 보냈으나 실패 시
+          }
+        } catch {
+          // axios 요청 실패 시
+        }
+      })();
+    }
+
+    localStorage.setItem("remainingTime", time);
+  }, [time, gameRoomId, userId]);
+
+  return { time, decreaseTime };
+};
+
+export default useTimer;
