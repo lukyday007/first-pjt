b1fe4add83225042204f640b868f119b32f385c0
최태민 <wxogud3377@naver.com>
2024-08-04 13:00:22 +0900

    <refactor: handler 처리방식 수정(연결시, 연결 종료, message)> S11P12B205-222

--- BackEnd/src/main/java/com/boricori/config/WebSocketHandler.java
+++ BackEnd/src/main/java/com/boricori/config/WebSocketHandler.java
@@ -10,7 +10,6 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -20,48 +19,12 @@ public class WebSocketHandler extends TextWebSocketHandler {
 
     @Autowired
     private GameRoomServiceImpl gameRoomService;
-    private ObjectMapper objectMapper = new ObjectMapper();
-    private static final Map<String, List<WebSocketSession>> roomSessions = new ConcurrentHashMap<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         String username = (String)session.getAttributes().get("username");
-        String roomId = session.getUri().getPath().split("/gameRoom/")[1];
-
-        List<String> players = gameRoomService.enterRoom(roomId, username);
-        roomSessions.compute(roomId, (key, sessions) -> {
-            if (sessions == null) {
-                sessions = new ArrayList<>();
-            }
-            synchronized (sessions) {
-                sessions.add(session);
-            }
-            return sessions;
-        });
-        ChangeListJsonAndSend(session, roomId, players);
-    }
-
-    @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        String username = (String)session.getAttributes().get("username");
-        String roomId = session.getUri().getPath().split("/gameRoom/")[1];
-        List<String> players = gameRoomService.leaveRoom(roomId, username);
-        roomSessions.get(roomId).remove(session);
-        ChangeListJsonAndSend(session, roomId, players);
-    }
-
-    private void ChangeListJsonAndSend(WebSocketSession originSession, String roomId, List<String> players) throws IOException {
-        List<WebSocketSession> sessions = roomSessions.get(roomId);
-        String jsonMessage = objectMapper.writeValueAsString(players);
-
-        if (sessions != null) {
-            synchronized (sessions) {
-                for (WebSocketSession session : sessions) {
-                    if (session.isOpen() && !session.getId().equals(originSession.getId())) {
-                        session.sendMessage(new TextMessage(jsonMessage));
-                    }
-                }
-            }
-        }
+        String roomId = session.getUri().getPath().split("/room/")[1];
+        String sessionId = session.getId();
+        gameRoomService.enterRoom(roomId, sessionId, username);
     }
 }
