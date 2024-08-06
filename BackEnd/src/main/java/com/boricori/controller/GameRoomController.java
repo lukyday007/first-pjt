41bdbefe5494e3df20aa58e6fef547336e828447
Yungsix <dbstjr9898@naver.com>
2024-08-06 09:18:39 +0900

    test/ TEST

--- BackEnd/src/main/java/com/boricori/controller/GameRoomController.java
+++ BackEnd/src/main/java/com/boricori/controller/GameRoomController.java
@@ -1,6 +1,8 @@
 package com.boricori.controller;
 
-import com.boricori.dto.ParticipantNameDto;
+import static com.boricori.util.ResponseEnum.FAIL;
+import static com.boricori.util.ResponseEnum.SUCCESS;
+
 import com.boricori.dto.request.gameroom.EndGameRoomRequest;
 import com.boricori.dto.request.gameroom.GameRequest;
 import com.boricori.dto.request.gameroom.StartGameRoomRequest;
@@ -10,7 +12,6 @@ import com.boricori.dto.response.gameroom.GameInfoResponse;
 import com.boricori.dto.response.gameroom.end.EndGameResponse;
 import com.boricori.entity.GameParticipants;
 import com.boricori.entity.GameRoom;
-import com.boricori.entity.Mission;
 import com.boricori.entity.User;
 import com.boricori.game.GameManager;
 import com.boricori.service.GameRoomService;
@@ -18,7 +19,6 @@ import com.boricori.service.InGameService;
 import com.boricori.service.MessageService;
 import com.boricori.service.ParticipantsService;
 import com.boricori.service.UserService;
-import com.boricori.util.Node;
 import com.boricori.util.ResponseEnum;
 import com.boricori.util.UserCircularLinkedList;
 import com.google.zxing.WriterException;
@@ -26,23 +26,22 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
 import jakarta.servlet.http.HttpServletRequest;
-
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import java.io.IOException;
-import static com.boricori.util.ResponseEnum.FAIL;
-import static com.boricori.util.ResponseEnum.SUCCESS;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/gameroom")
 @RestController
@@ -95,17 +94,18 @@ public class GameRoomController {
       @ApiResponse(responseCode = "404", description = "실패"),
   })
   public ResponseEntity<EnterRoomResponse> enterGameRoom(
-      @PathVariable @Parameter(description = "방 입장 코드", required = true) String gameCode, HttpServletRequest req) {
+      @PathVariable @Parameter(description = "방 입장 코드", required = true) String gameCode,
+      HttpServletRequest req) {
     String username = (String) req.getAttribute("username");
     GameRoom game = gameRoomService.findGameByCode(gameCode);
-      int maxCount = game.getMaxPlayer();
-      int currCount = gameRoomService.getCurrentRoomPlayerCount(String.valueOf(game.getId()));
+    int maxCount = game.getMaxPlayer();
+    int currCount = gameRoomService.getCurrentRoomPlayerCount(String.valueOf(game.getId()));
 //    int currCount = 1;
-      if(currCount >= maxCount){
-        return ResponseEntity.status(FAIL.getCode()).body(new EnterRoomResponse("Full", null));
-      }
-      return ResponseEntity.status(SUCCESS.getCode()).body(new EnterRoomResponse("Available",
-          game.getId()));
+    if (currCount >= maxCount) {
+      return ResponseEntity.status(FAIL.getCode()).body(new EnterRoomResponse("Full", null));
+    }
+    return ResponseEntity.status(SUCCESS.getCode()).body(new EnterRoomResponse("Available",
+        game.getId()));
   }
 
   @PostMapping("/{id}/start")
@@ -116,16 +116,17 @@ public class GameRoomController {
       @ApiResponse(responseCode = "500", description = "서버 오류")
   })
   public ResponseEntity<String> startGameRoom(
-          @PathVariable @Parameter(description = "게임 방id", required = true) long id,
+      @PathVariable @Parameter(description = "게임 방id", required = true) long id,
       @RequestBody @Parameter(description = "게임 시작 서버 데이터 전달", required = true) StartGameRoomRequest request,
       HttpServletRequest req) {
 
     String username = (String) req.getAttribute("username");
     // 게임 방 튜플 생성
     GameRoom gameRoom = gameRoomService.updateRoom(id, request);
-    List<String> usernames = gameRoomService.GameRoomPlayerAll(String.valueOf(id)); // Redis에서 불러올 부분
+    List<String> usernames = gameRoomService.GameRoomPlayerAll(
+        String.valueOf(id)); // Redis에서 불러올 부분
     List<User> users = new ArrayList<>();
-    for (String u : usernames){
+    for (String u : usernames) {
       users.add(userService.findByUsername(u));
     }
     users.forEach(u -> participantsService.addRecord(
@@ -134,8 +135,10 @@ public class GameRoomController {
     makeCatchableList(gameRoom.getId(), users);
     // 알림 시간 Redis에 넣기
     int interval = gameRoom.getGameTime() * 60 / 4;
-    for (int t = 1; t < 5; t++){
-      redisTemplate.opsForValue().set(String.format("%d-%d", gameRoom.getId(), t), String.valueOf(t), interval * t, TimeUnit.SECONDS);
+    for (int t = 1; t < 5; t++) {
+      redisTemplate.opsForValue()
+          .set(String.format("%d-%d", gameRoom.getId(), t), String.valueOf(t), interval * t,
+              TimeUnit.SECONDS);
     }
     messageService.readyGame(gameRoom.getId(), gameRoom);
     // 3초 대기
@@ -161,7 +164,8 @@ public class GameRoomController {
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "OK"),
   })
-  public ResponseEntity<GameInfoResponse> enterGame(@PathVariable Long gameId, HttpServletRequest req) {
+  public ResponseEntity<GameInfoResponse> enterGame(@PathVariable Long gameId,
+      HttpServletRequest req) {
     String username = (String) req.getAttribute("username");
     User target = GameManager.catchableList.get(gameId).getByUsername(username).next.data;
     GameRoom game = gameRoomService.findGame(gameId);
