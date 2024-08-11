5b88cdf02c6fdaf48d85fb10859cd33e3b7e35b8
unknown <iiky2124@gmail.com>
2024-08-12 01:19:48 +0900

    fix: error 수정 위한 console.log

--- FrontEnd/src/hooks/Map/useStartGame.jsx
+++ FrontEnd/src/hooks/Map/useStartGame.jsx
@@ -2,7 +2,6 @@ import { useState, useContext } from "react";
 import { GameContext } from "@/context/GameContext";
 import useBullet from "@/hooks/Map/useBullet";
 import axiosInstance from "@/api/axiosInstance";
-import { meta } from "eslint-plugin-prettier";
 
 // GamePlay.jsx에서 시작 프로세스 관리
 const useStartGame = () => {
@@ -26,10 +25,13 @@ const useStartGame = () => {
   };
 
   // 게임 시작 시간 처리 함수 별도 분리
-  const handleGameStart = newStartTime => {
+  const handleStartGameTime = newStartTime => {
     const startTime = new Date(newStartTime).getTime();
     const currentTime = new Date().getTime();
     const initialTimeUntilStart = startTime - currentTime; // 게임 시작까지 남은 시간, ms 단위
+    console.log(`startTime: ${startTime}`);
+    console.log(`currentTime: ${currentTime}`);
+    console.log(`initialTimeUntilStart: ${initialTimeUntilStart}`);
     setTimeUntilStart(initialTimeUntilStart);
 
     // 대기 시간 동안 1초마다 남은 시간 계산
@@ -58,11 +60,15 @@ const useStartGame = () => {
   };
 
   const fetch = async () => {
+    console.log("fetch 함수 호출됨");
     try {
       const response = await axiosInstance.get(`/in-game/init/${gameRoomId}`);
+      console.log(`response.status: ${response.status}`);
 
       if (response.status == 200) {
+        console.log("데이터 정상 수신");
         const metadata = response.data;
+        console.log(`metadata: ${metadata}`);
 
         // 반경, 중심, 타겟 닉네임 수신
         const newAreaRadius = parseInt(metadata.gameInfo.mapSize, 10);
@@ -88,12 +94,13 @@ const useStartGame = () => {
         updateMissionAndItems(metadata.myMissions, metadata.myItems);
 
         // 게임 시작 시간 처리
-        handleGameStart(metadata.gameInfo.startTime);
+        handleStartGameTime(metadata.gameInfo.startTime);
       } else {
         alert(`요청 수신 중 문제가 발생했습니다: ${response.status}`);
       }
-    } catch {
+    } catch (error) {
       alert("요청을 보내는 중 문제가 발생했습니다.");
+      console.log(error);
     }
   };
 
