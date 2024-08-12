778158f14664043f7671d77c055acfbf1470dad2
1139091SSAFY <iiky2124@gmail.com>
2024-08-12 21:06:44 +0900

    fix: getBullet 수정

--- FrontEnd/src/hooks/Map/useBullet.jsx
+++ FrontEnd/src/hooks/Map/useBullet.jsx
@@ -8,8 +8,8 @@ const useBullet = () => {
   const [isCooldown, setIsCooldown] = useState(false); // 재사용 대기시간 여부
   const COOLDOWN = 3000; // 재발사 시간 3초
 
-  const getBullet = () => {
-    setBullet(prev => prev + 1);
+  const getBullet = n => {
+    setBullet(prev => prev + n);
   };
 
   const shootBullet = () => {
