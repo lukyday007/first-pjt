9107a65ab84ae3c95552d5447903b9440f46ecfa
이규빈 <kyubin.dev@gmail.com>
2024-08-12 16:14:06 +0900

    fix: 오류 수정

--- FrontEnd/src/components/ProfileDialog.jsx
+++ FrontEnd/src/components/ProfileDialog.jsx
@@ -11,13 +11,9 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/Avatar";
 
 const ProfileDialog = ({ isOpen, onClose, apiPath, isMyProfile }) => {
   const [profileData, setProfileData] = useState(null);
-  // const { username, email, playtime, profilePic } = profileData || {};
+  const { username, email, playtime, profilePic } = profileData || {};
 
-  // 테스트 데이터
-  const username = "테스트";
-  const email = "test@naver.com";
-  const playtime = "30";
-  const profilePic = useEffect(() => {
+  useEffect(() => {
     if (isOpen) {
       const fetchProfile = async () => {
         try {
@@ -41,7 +37,7 @@ const ProfileDialog = ({ isOpen, onClose, apiPath, isMyProfile }) => {
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="h-auto w-[80%] p-8">
+      <DialogContent className="h-auto w-[80%] rounded-2xl p-8">
         <DialogHeader>
           <DialogTitle className="text-lg">
             {isMyProfile === "true" ? "내 정보" : "유저 정보"}
@@ -50,7 +46,7 @@ const ProfileDialog = ({ isOpen, onClose, apiPath, isMyProfile }) => {
         <hr />
         <div className="flex flex-col items-center justify-center">
           <Avatar className="mb-4 h-32 w-32">
-            <AvatarImage src="" alt="avatar" />
+            <AvatarImage src={profilePic} alt="avatar" />
             <AvatarFallback>{username}</AvatarFallback>
           </Avatar>
           <div className="mb-8 text-2xl font-bold">{username}</div>
