95d60aa2fb948da781ff98e9bf02bf8e2da551d7
최태민 <wxogud3377@naver.com>
2024-08-12 14:12:28 +0900

    <refactor: 스코어 순 랭킹시스템> S11P12B205-243

--- BackEnd/src/main/java/com/boricori/dto/response/User/RankDtoResponse.java
+++ BackEnd/src/main/java/com/boricori/dto/response/User/RankDtoResponse.java
@@ -1,26 +1,23 @@
 package com.boricori.dto.response.User;
 
 
-import com.boricori.entity.User;
+import com.querydsl.core.annotations.QueryProjection;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 @Schema(name = "유저 랭크")
-@Getter
-@Setter
+@Data
 public class RankDtoResponse {
 
-    @Schema(description = "순위")
-    int rank;
     @Schema(description = "유저 아이디")
     String username;
     @Schema(description = "누적 점수")
     int score;
 
-    public RankDtoResponse(int rank, User user) {
-        this.rank = rank;
-        this.username = user.getUsername();
-        this.score = user.getScores();
+    @QueryProjection
+    public RankDtoResponse(String username, int score) {
+        this.username = username;
+        this.score = score;
     }
 }
+
