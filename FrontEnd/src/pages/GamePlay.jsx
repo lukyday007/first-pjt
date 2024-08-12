68d2b4033e7eea00310a66f8d1ab058efcd372aa
dayullee <dy007lukyday@gmail.com>
2024-08-12 13:18:19 +0900

    update: openvidu url 두번쩨

--- FrontEnd/src/pages/GamePlay.jsx
+++ FrontEnd/src/pages/GamePlay.jsx
@@ -272,7 +272,7 @@ const GamePlay = () => {
 
   const createSession = async sessionId => {
     const response = await axios.post(
-      APPLICATION_SERVER_URL + "sessions",
+      APPLICATION_SERVER_URL + "/sessions",
       { customSessionId: sessionId },
       {
         headers: { "Content-Type": "application/json" },
@@ -283,7 +283,7 @@ const GamePlay = () => {
 
   const createToken = async sessionId => {
     const response = await axios.post(
-      APPLICATION_SERVER_URL + "sessions/" + sessionId + "/connections",
+      APPLICATION_SERVER_URL + "/sessions/" + sessionId + "/connections",
       {},
       {
         headers: { "Content-Type": "application/json" },
