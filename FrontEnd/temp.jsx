27b9eb9abe1d6adbcd52fc574f5d9a875576cfda
1139091SSAFY <iiky2124@gmail.com>
2024-08-14 23:05:48 +0900

    fix: end msgType 수신 데이터 형식 수정

---
+++ FrontEnd/temp.jsx
@@ -0,0 +1,1 @@
+{"msgType":"end", "data":"{"gameId":79,"winner1":"큰두터비","winner2":"작은두터비","result":[{"userName":"큰두터비","missionComplete":0,"kills":0,"score":0},{"userName":"작은두터비","missionComplete":0,"kills":0,"score":0},{"userName":"두터비","missionComplete":0,"kills":0,"score":0}]}"}
